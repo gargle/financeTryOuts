fea432c0cc5957608d288dfe3daaf0c26a5d06b4
Johan Laenen <johan.laenen@bpost.be>
2020-08-05 14:50:49 +0200

    fin.pv deprecated, watch out

--- main-tests.py
+++ main-tests.py
@@ -1,5 +1,6 @@
 import unittest
 from main import main
+import numpy.lib.financial as fin
 
 class mainTests(unittest.TestCase):
     #def setup(self):
@@ -7,7 +8,11 @@ class mainTests(unittest.TestCase):
     #def tearDown(self):
 
     def testFv(self):
-        self.assertEquals(main.pv(main.fv(100,0.02,2),0.02,2), 100.00)
+        self.assertEqual(main.pv(-fin.fv(0.02,2,0,100),0.02,2), 100.00)
+        self.assertEqual(-fin.pv(0.02,2,0,main.fv(100,0.02,2)), 100.00)
+        self.assertEqual(main.pv(main.fv(100,0.02,2),0.02,2), 100.00)
+
+
 
 if __name__ == "__main__":
     unittest.main()
